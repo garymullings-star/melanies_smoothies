7560a7639d247bbc86ff45de712758f66c4f33b6
garymullings-star <garymullings@gmail.com>
2026-06-07 22:38:56 -0400

    Update streamlit_app.py
    Connected changes needed bc of move from SIS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: {st.__version__}")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your 
   custom Smoothie
